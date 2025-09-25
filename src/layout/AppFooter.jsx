2cfda75c5d7ccca74e1650b8272f6e598319e97c
RajTruact <raj@techpit.in>
2025-09-25 09:33:41 +0530

    c

--- src/layout/AppFooter.jsx
+++ src/layout/AppFooter.jsx
@@ -4,7 +4,7 @@ const AppFooter = () => {
   return (
     <footer className="sticky bottom-0 w-full bg-white z-50 text-gray-700 dark:text-white dark:bg-[#16181D] h-[40px] border-t-2 dark:border-gray-800 border-white">
       <p className="text-center flex justify-center items-center h-full text-sm">
-        TruAct | All Rights Reserved
+        © 2025 TruAct. All Rights Reserved.
       </p>
     </footer>
   );

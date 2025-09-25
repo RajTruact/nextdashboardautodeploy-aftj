231741810bb9c5d29aa1f81f7fedcbe08795c54c
rajgupta2399 <rajkumardotnet2499@gmail.com>
2025-09-25 23:33:41 +0530

    c

--- src/layout/AppFooter.jsx
+++ src/layout/AppFooter.jsx
@@ -6,20 +6,21 @@ import { usePathname } from "next/navigation";
 const AppFooter = () => {
   const pathname = usePathname();
   const roles = pathname.split("/")[1];
+  
   return (
-    <footer className="sticky bottom-0 w-full bg-white z-50 text-gray-700 dark:text-white dark:bg-[#16181D] h-[40px] border-t-2 dark:border-gray-800 border-white">
-      <div className="flex justify-center items-center gap-1 mt-2.5">
-        <p className="text-center flex justify-center items-center h-full text-[13px]">
-          © 2025 TruAct. All Rights Reserved.
+    <footer className="sticky bottom-0 w-full bg-white z-50 text-[#D0D5DD] dark:text-[#D0D5DD] dark:bg-[#16181D] h-[40px] border-t-2 dark:border-gray-800 border-white">
+      <div className="flex justify-center items-center h-full gap-1">
+        <p className="text-center text-[13px]">
+          © 2025 TruAct. {" " } All Rights Reserved.
         </p>
         <Link
           href={`/${roles}/support`}
-          className="text-center flex justify-center items-center h-full text-[13px]"
+          className="text-center text-[13px] hover:underline transition-all"
         >
           Support
         </Link>
-        <p className="text-center flex justify-center items-center h-full text-[13px]">
-          1.7
+        <p className="text-center text-[13px]">
+          v1.7
         </p>
       </div>
     </footer>

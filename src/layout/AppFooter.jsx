5e271feb15ec4815d69e814109f7a916c524a57f
RajTruact <raj@techpit.in>
2025-09-26 16:28:52 +0530

    c

--- src/layout/AppFooter.jsx
+++ src/layout/AppFooter.jsx
@@ -6,22 +6,32 @@ import { usePathname } from "next/navigation";
 const AppFooter = () => {
   const pathname = usePathname();
   const roles = pathname.split("/")[1];
-  
+
   return (
-    <footer className="sticky bottom-0 w-full bg-white z-50 text-[#D0D5DD] dark:text-[#D0D5DD] dark:bg-[#16181D] h-[40px] border-t-2 dark:border-gray-800 border-white">
-      <div className="flex justify-center items-center h-full gap-1">
-        <p className="text-center text-[13px]">
-          © 2025 TruAct. {" " } All Rights Reserved.
-        </p>
+    <footer className="sticky bottom-0 w-full bg-white z-50  dark:text-[#cbcfd4] dark:bg-[#16181D] h-[40px] border-t-2 dark:border-gray-800 border-white">
+      <div className="flex justify-center items-center h-full gap-3 sm:gap-5">
+        <p className="text-center text-[13px]">© 2025 TruAct</p>
+        <p className="text-center text-[13px]">All Rights Reserved</p>
+        <Link
+          href={`/${roles}/terms`}
+          className="text-center text-[13px] hover:underline transition-all"
+        >
+          Terms
+        </Link>
+        <Link
+          href={`/${roles}/privacy`}
+          className="text-center text-[13px] hover:underline transition-all"
+        >
+          Privacy
+        </Link>
+
         <Link
           href={`/${roles}/support`}
           className="text-center text-[13px] hover:underline transition-all"
         >
           Support
         </Link>
-        <p className="text-center text-[13px]">
-          v1.7
-        </p>
+        <p className="text-center text-[13px]">v1.0.0</p>
       </div>
     </footer>
   );

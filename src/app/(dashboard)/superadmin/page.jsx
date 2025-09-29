d08cac7e319ec1a217007272a59e0319a731cc29
RajTruact <raj@techpit.in>
2025-09-29 16:27:26 +0530

    c

--- src/app/(dashboard)/superadmin/page.jsx
+++ src/app/(dashboard)/superadmin/page.jsx
@@ -4,71 +4,8 @@ export default function SuperAdminDashboard() {
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12">
         <h1 className="text-md font-bold text-gray-900 dark:text-white">
-          Super Admin Dashboard
+          Form Page
         </h1>
-        <p className="text-gray-600 dark:text-gray-400 mt-2">
-          Welcome Super Admin
-        </p>
-        <p className="text-gray-600 dark:text-gray-400 mt-2 flex justify-center items-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quas
-          labore atque placeat nesciunt? Eligendi fugiat excepturi consequatur,
-          obcaecati architecto deleniti perferendis id ratione commodi ut ad
-          provident facilis cupiditate corrupti voluptatum, veritatis quia. Rem,
-          nostrum autem ea veniam magnam tempore necessitatibus ad nisi neque
-          exercitationem ab. Ipsa, beatae quos sapiente, optio, ad corrupti
-          cupiditate quidem obcaecati dolorem porro molestiae! Omnis veniam
-          molestias quod laudantium quisquam corrupti sint accusantium quia quae
-          maiores. Officiis quidem aliquam nulla fuga quae deserunt perferendis
-          accusamus cupiditate nobis, exercitationem dolores voluptatum
-          consequuntur dolor enim, harum nemo. Suscipit repellendus dolorem
-          inventore ullam ad officia explicabo similique minus soluta
-          consequuntur. Quidem dolorum laudantium rem perferendis, eaque
-          expedita? Earum cumque repellat doloremque amet molestias ut
-          accusantium eos maiores quia autem, placeat accusamus? Natus numquam
-          consectetur placeat rerum odio? Voluptatem enim eaque consequatur,
-          officiis nemo facilis possimus dolorum unde doloremque harum quam ad?
-          Officia quas dicta incidunt eos aperiam error, consequatur quos
-          quibusdam. Culpa, magni dolor. Ratione voluptates modi obcaecati quos
-          quod? Odio, incidunt, perferendis illo beatae provident assumenda
-          expedita inventore ipsum commodi quisquam praesentium. Sit vel quos
-          dolorum eligendi quisquam doloribus, doloremque consequuntur natus,
-          nam accusantium deleniti reiciendis quia repellat nemo eos repellendus
-          iure eius! Deserunt rerum facilis suscipit debitis illo placeat
-          tenetur mollitia necessitatibus. Sint ipsam aut enim assumenda
-          incidunt iste aspernatur facilis quasi commodi odio, praesentium
-          cumque velit sequi. Sit obcaecati ratione consequatur pariatur dolore
-          numquam quam? Ex, nam vel. Sint, mollitia quasi. Necessitatibus, totam
-          ducimus recusandae provident blanditiis, nam saepe nostrum autem
-          reiciendis, magni dignissimos deleniti perferendis nihil laboriosam
-          corrupti. Laborum, placeat doloribus blanditiis ducimus voluptas
-          accusamus consectetur. Porro temporibus quia neque distinctio nemo,
-          earum fuga quod doloribus perferendis? Consequatur nobis illum id
-          doloremque modi sequi aliquid odio beatae, quibusdam dicta quam amet,
-          quisquam, incidunt alias atque illo optio ipsa debitis molestias
-          similique natus quia explicabo? Dolorum quasi maiores sint accusantium
-          iure perferendis architecto illo praesentium, magnam ducimus ipsa
-          cupiditate. Vero facere saepe nostrum veniam eum nulla possimus,
-          repellat placeat sequi facilis minus magnam quisquam, quos distinctio,
-          ipsam ad repellendus pariatur. Praesentium ratione saepe dolores autem
-          vel laudantium, vero cumque quos. Odit aperiam quam eaque consectetur
-          rerum veniam voluptatum molestias repellat nobis. Placeat molestiae et
-          perferendis alias odio iste accusantium nesciunt ut soluta fugit,
-          culpa fugiat. Eum quaerat vel voluptatibus, cupiditate reprehenderit
-          obcaecati ad officiis eaque laudantium hic pariatur, fugit eveniet,
-          itaque aliquam suscipit tenetur debitis! Laboriosam incidunt culpa
-          accusantium, rem a sapiente consequuntur aut, doloribus esse fuga
-          officiis vero ut tenetur quis quia excepturi molestiae asperiores quos
-          voluptatem reprehenderit ullam laudantium, ipsam soluta ducimus?
-          Aliquam incidunt quo enim numquam debitis? Aperiam eius similique
-          perspiciatis consequatur doloremque sint facere. Voluptates odio sunt
-          quis ex rerum earum expedita vero iure fuga magni! Voluptatem cum,
-          unde suscipit sequi mollitia doloremque quod in pariatur perspiciatis
-          quos omnis tempore dolores molestiae et iste! Cupiditate expedita
-          ipsam alias rem? Corrupti ab minima neque distinctio doloremque
-          officiis pariatur. Id in, praesentium, iusto ipsa optio rem autem
-          blanditiis a nemo fugiat assumenda eaque voluptatum neque repellat
-          sunt earum, velit veniam consequuntur illo!
-        </p>
         <p className="text-gray-600 dark:text-gray-400 mt-2 flex justify-center items-center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
           animi ut sint! Odit, sint fuga excepturi dolores fugit maxime

92a2b5049deca63be306e65026b87f921a10e886
RajTruact <raj@techpit.in>
2025-11-13 11:39:28 +0530

    c

--- src/app/(dashboard)/superadmin/faculty/page.jsx
+++ src/app/(dashboard)/superadmin/faculty/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>Faculty Page</div>;
+  return (
+    <div>
+      <h2>Faculty Raj</h2>
+    </div>
+  );
 };
 
 export default page;
